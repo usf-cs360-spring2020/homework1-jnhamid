7519f999b72de27a18fa5ab41e04451936288fad
Jnhamid <jnhamid@dons.usfca.edu>
2020-02-12 18:09:39 -0800

    Stack bar chart

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function drawBar(data){
 
-  dataset = Array.from(map.values()).sort();
+  // dataset = Array.from(map.values()).sort();
   //SVG attr
   var svgWidth = 960, svgHeight = 500, barPadding =5;
+
+  var keys = data.columns.slice(1);
+
+  var month = [...new Set(data.map(d => d.Month))];
+
 
   var svg = d3.select("svg")
   .attr("width", svgWidth)
@@ -21,110 +26,71 @@ function drawBar(data){
   bottom: 30, // leave space for x-axis
   left:   60 // leave space for y-axis
 };
-
 // now we can calculate how much space we have to plot
 let bounds = svg.node().getBoundingClientRect();
 let plotWidth = bounds.width - margin.right - margin.left;
 console.log(plotWidth)
 let plotHeight = bounds.height - margin.top - margin.bottom;
-var barWidth = (plotWidth / dataset.length);
-
-
-
+// var barWidth = (plotWidth / dataset.length);
 let plot = svg.append("g").attr("id", "plot");
 plot.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  //yScaler..
-  var yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataset)])
-  .range([0,plotHeight]).nice();
+var x = d3.scaleBand()
+		.range([margin.left, plotWidth - margin.right])
+		.padding(0.1)
 
-  var yScale2 = d3.scaleLinear()
-  .domain([0, d3.max(dataset)])
-  .range([plotHeight,0]).nice();
+var y = d3.scaleLinear()
+		.rangeRound([plotHeight - margin.bottom, margin.top])
 
-  var monthScale = d3.scaleTime()
-  .domain([new Date("2019-01-01"), new Date("2019-09-31")])
-  .rangeRound([0, plotWidth]);
+var xAxis = svg.append("g")
+		.attr("transform", `translate(0,${plotHeight - margin.bottom})`)
+		.attr("class", "x-axis")
 
+var yAxis = svg.append("g")
+		.attr("transform", `translate(${margin.left},0)`)
+		.attr("class", "y-axis")
 
-
-  let xAxis = d3.axisBottom(monthScale);
-  let yAxis = d3.axisLeft(yScale2);
-
-  let xGroup = plot.append("g").attr("id", "x-axis");
-  xGroup.call(xAxis);
-
-  // notice it is at the top of our svg
-  // we need to translate/shift it down to the bottom
-  xGroup.attr("transform", "translate(0," + plotHeight + ")");
+var z = d3.scaleOrdinal()
+		.range(["steelblue", "darkorange"])
+		.domain(keys);
 
 
-  let yGroup = plot.append("g").attr("id", "y-axis");
-  yGroup.call(yAxis);
-  // yGroup.attr("transform", "translate(0"+ "," + plotWidth + ")");
+var dataset = data.filter(f => f.Month)
 
-  let pairs = Array.from(map.entries());
-  console.log(pairs);
+		dataset.forEach(function(d) {
+			d.total = d3.sum(keys, k => +d[k])
+			return d
+		})
 
-  let bars = plot.selectAll("rect")
-   .data(pairs, function(d) { return d[1]; });
+    y.domain([0, d3.max(dataset, d => d3.sum(keys, k => +d[k]))]).nice();
 
+		svg.selectAll(".y-axis")
+			.call(d3.axisLeft(y).ticks(null, ".1s"))
 
-   bars.enter().append("rect")
-  // we will style using css
-  .attr("class", "bar")
-  // the width of our bar is determined by our band scale
-  .attr("width", barWidth - barPadding)
-  // we must now map our letter to an x pixel position
-  // note the use of arrow functions here
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Arrow_functions
-  .attr("x", d => monthScale(d[0]))
-  // and do something similar for our y pixel position
-  .attr("y", d => yScale2(d[1]))
-  // here it gets weird again, how do we set the bar height?
-  .attr("height", d => plotHeight - yScale2(d[1]))
-  .attr("transform", function(d, i){
-    var translate = [(barWidth * (i) + 5), 0];
-    return "translate("+translate+")";})
-  .each(function(d, i, nodes) {
-    console.log("Added bar for:", d[0]);
-  });
+    x.domain(dataset.map(d => d.Month));
 
+		svg.selectAll(".x-axis")
+			.call(d3.axisBottom(x).tickSizeOuter(0))
 
+      var group = svg.selectAll("g.layer")
+			.data(d3.stack().keys(keys)(dataset), d => d.key);
 
+      group.exit().remove()
 
-//   var barChart = plot.selectAll("rect")
-//     .data(dataset)
-//     .enter()
-//     .append("rect")
-//     .attr("y", function(d){
-//       return plotHeight - yScale(d);
-//     })
-//     .attr("height", function(d){
-//       return yScale(d);
-//     })
-//     .attr("width", barWidth - barPadding )
-//     .attr("transform", function(d, i){
-//       var translate = [(barWidth * (i) + 5), 0];
-//       return "translate("+translate+")";
-//   });
+      group.enter().append("g")
+			.classed("layer", true)
+			.attr("fill", d => z(d.key));
+
+		var bars = svg.selectAll("g.layer").selectAll("rect")
+			.data(d => d, e => console.log(e.data.Month));
+      bars.exit().remove()
+
+	bars.enter().append("rect")
+			.attr("width", x.bandwidth())
+			.merge(bars)
+			.attr("x", d => x(d.data.Month))
+			.attr("y", d => y(d[1]))
+			.attr("height", d => y(d[0]) - y(d[1]))
+
 }
-
-//Maps for holding data
-let mapI = new Map();
-let mapD = new Map();
-let map = new Map();
-
-d3.csv("test.csv", function(data){
-  if(map.has(data.Month)){
-    passcount = map.get(data.Month)
-
-    passcount += parseInt(data.Count)
-    map.set(data.Month, passcount)
-
-  }else {
-    map.set(data.Month, parseInt(data.Count))
-  }
-}).then(function(){
-  drawBar()});
+d3.csv("test2.csv").then(d => drawBar(d));
---
+++ stackedtest.js
@@ -0,0 +1,137 @@
+function drawBar(data){
+
+  dataset = Array.from(map.values()).sort();
+  //SVG attr
+  var svgWidth = 960, svgHeight = 500, barPadding =5;
+
+  var keys = data.columns.slice(1);
+  console.log(keys)
+
+  var svg = d3.select("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+
+  svg.append("text").attr("id", "charttitle")
+   .attr("x",  svgWidth/3.9 )
+   .attr("y", 25)
+   .style("text-anchor", "middle")
+   .text("Number of people flying out of sfo");
+
+  let margin = {
+  top:    15,
+  right:  10,
+  bottom: 30, // leave space for x-axis
+  left:   60 // leave space for y-axis
+};
+
+// now we can calculate how much space we have to plot
+let bounds = svg.node().getBoundingClientRect();
+let plotWidth = bounds.width - margin.right - margin.left;
+console.log(plotWidth)
+let plotHeight = bounds.height - margin.top - margin.bottom;
+var barWidth = (plotWidth / dataset.length);
+
+
+
+let plot = svg.append("g").attr("id", "plot");
+plot.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //yScaler..
+  var yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0,plotHeight]).nice();
+
+  var yScale2 = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([plotHeight,0]).nice();
+
+  var monthScale = d3.scaleTime()
+  .domain([new Date("2019-01-01"), new Date("2019-09-31")])
+  .rangeRound([0, plotWidth]);
+
+  var z = d3.scaleOrdinal()
+    .range(["steelblue", "darkorange"])
+    .domain(keys);
+
+
+
+
+  let xAxis = d3.axisBottom(monthScale);
+  let yAxis = d3.axisLeft(yScale2).ticks(10, ".1s");
+
+  let xGroup = plot.append("g").attr("id", "x-axis");
+  xGroup.call(xAxis);
+
+  // notice it is at the top of our svg
+  // we need to translate/shift it down to the bottom
+  xGroup.attr("transform", "translate(0," + plotHeight + ")");
+
+
+  let yGroup = plot.append("g").attr("id", "y-axis");
+  yGroup.call(yAxis);
+  // yGroup.attr("transform", "translate(0"+ "," + plotWidth + ")");
+
+  let pairs = Array.from(map.entries());
+  console.log(pairs);
+
+  let bars = plot.selectAll("rect")
+   .data(pairs, function(d) { return d[1]; });
+
+
+   bars.enter().append("rect")
+  // we will style using css
+  .attr("class", "bar")
+  // the width of our bar is determined by our band scale
+  .attr("width", barWidth - barPadding)
+  // we must now map our letter to an x pixel position
+  // note the use of arrow functions here
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Arrow_functions
+  .attr("x", d => monthScale(d[0]))
+  // and do something similar for our y pixel position
+  .attr("y", d => yScale2(d[1]))
+  // here it gets weird again, how do we set the bar height?
+  .attr("height", d => plotHeight - yScale2(d[1]))
+  .attr("transform", function(d, i){
+    var translate = [(barWidth * (i) + 5), 0];
+    return "translate("+translate+")";})
+  .each(function(d, i, nodes) {
+    console.log("Added bar for:", d[0]);
+  });
+
+
+
+
+//   var barChart = plot.selectAll("rect")
+//     .data(dataset)
+//     .enter()
+//     .append("rect")
+//     .attr("y", function(d){
+//       return plotHeight - yScale(d);
+//     })
+//     .attr("height", function(d){
+//       return yScale(d);
+//     })
+//     .attr("width", barWidth - barPadding )
+//     .attr("transform", function(d, i){
+//       var translate = [(barWidth * (i) + 5), 0];
+//       return "translate("+translate+")";
+//   });
+}
+
+//Maps for holding data
+let mapI = new Map();
+let mapD = new Map();
+let map = new Map();
+
+d3.csv("test2.csv", function(data){
+  //   if(map.has(data.Month)){
+  //     passcount = map.get(data.Month)
+  //
+  //     passcount += parseInt(data.Count)
+  //     map.set(data.Month, passcount)
+  //
+  //   }else {
+  //     map.set(data.Month, parseInt(data.Count))
+  // }
+}).then(d => drawBar(d));
